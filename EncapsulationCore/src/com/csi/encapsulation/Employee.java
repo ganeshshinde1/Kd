85d8aa3797b2d059b39846bab1fe3eb3e0666a56
ganeshshinde1 <ganeshshinde2681998@gmail.com>
2021-09-15 04:50:22 -0700

    wowww

---
+++ EncapsulationCore/src/com/csi/encapsulation/Employee.java
@@ -0,0 +1,25 @@
+package com.csi.encapsulation;
+
+public class Employee {
+private int empId;
+private String empname;
+public int getEmpId() {
+	return empId;
+// added comments	
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getEmpname() {
+	return empname;
+}
+public void setEmpname(String empname) {
+	this.empname = empname;
+}
+public static void main(String[] args) {
+	Employee  e1 = new Employee();
+	e1.setEmpId(121);
+	e1.setEmpname("SWARA");
+	System.out.println("\n Employee Id:"+ e1.getEmpId() + "\n Employee Name:"+ e1.getEmpname());
+}
+}
